1766e2c09fc637900ddf828cfc05e1ac555f02e8
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-19 15:56:59 +0530

    supermarket discount calculation for Chef
    Implement logic for calculating the total cost after applying the discount for the lowest priced item.

---
+++ Q9.py
@@ -0,0 +1,12 @@
+#Chef goes to the supermarket to buy some items. Luckily there's a sale going on under which Chef gets the following offer:
+#If Chef buys 3 items then he gets the item (out of those 3 items) having the lowest price as free.
+#For e.g. if Chef bought 3 items with the cost 6, 2 and 4, then he would get the item with cost 
+#2 as free. So he would only have to pay the cost of the other two items which will be 6+4=10
+#Chef buys 3 items having prices A, B and C respectively. What is the amount of money Chef needs to pay?
+
+
+
+t=int(input())
+for i in range(t):
+    A,B,C=map(int,input().split())
+    print(A+B+C-min(A,B,C))
