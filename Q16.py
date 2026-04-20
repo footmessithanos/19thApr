a4b3b73f231a8de7583501253e04515928bdda56
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-20 17:21:09 +0530

    Add program to process test cases and increment values

---
+++ Q16.py
@@ -0,0 +1,9 @@
+#Write a program that takes an integer T for number of test cases as input, then for each test case reads an integer N on next T lines, and prints N + 1 for each test case
+
+
+
+
+t=int(input())
+for i in range(t):
+    N=int(input())
+    print(N+1)
