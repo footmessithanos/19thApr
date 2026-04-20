b8706228e467b1acc4d1225f8301785670c8c3b7
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-20 16:54:58 +0530

    Add element existence check in array

---
+++ Q11.py
@@ -0,0 +1,10 @@
+#You are given an array A of size N and an element X. Your task is to find whether the array A contains the element X or not.
+
+
+
+N,X=map(int,input().split())
+A=list(map(int,input().split()))
+if X in A:
+    print("YES")
+else: 
+    print("NO")
