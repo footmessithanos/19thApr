a86798ab7ff494be38dba6c2df8508f39850bc8d
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-20 17:28:09 +0530

    Add functions to calculate black cells in chessboard

---
+++ Q18.py
@@ -0,0 +1,21 @@
+#Given n (n is even), determine the number of black cells in an n×n chessboard. Update the function given in the IDE to solve the problem.
+
+
+
+
+
+
+def square(t):
+    return n*n
+
+
+    
+def half(i):
+    return i//2
+ 
+ 
+ 
+n = int(input())
+result1 = square(n)
+result2 = half(result1)
+print(result2)
