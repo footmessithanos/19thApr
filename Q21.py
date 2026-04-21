2ce9c241ec1e1ea895d99193c6c0999127d81156
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-21 16:04:33 +0530

    Fix comment formatting in Q21.py

---
+++ Q21.py
@@ -0,0 +1,9 @@
+#Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is X km away from his home.
+#If his office is open on 5 days in a week, find the number of kilometers Chef travels through office trips in a week.
+
+
+
+t=int(input())
+for x in range(t):
+    X=int(input())
+    print(X*10)
