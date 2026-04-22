444ced8ae887ffffb9f7a2bb366f384abdb9b66f
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-22 16:29:25 +0530

    Add arithmetic progression check for integers A and C

---
+++ Q31.py
@@ -0,0 +1,14 @@
+#Chef is given two integers A and C such that A≤C. Chef wants to find whether there exists any integer B such that A,B, and C are in arithmetic progression.
+#For each test case, output −1 if there exists no integer B such that A,B, and C are in arithmetic progression. Else, output the value of B.
+
+
+
+
+t=int(input())
+for i in range(t):
+    A,C=map(int,input().split())
+    if (A+C)%2==0:
+        print((A+C)//2)
+    else:
+        print(-1)
+
